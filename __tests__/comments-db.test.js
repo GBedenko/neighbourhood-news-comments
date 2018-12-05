2649afe9dab0ea6fb9469ac82de44b2c29842ae0
Genaro Bedenko (bedenkog) <bedenkog@coventry.ac.uk>
2018-12-05 11:28:02 +0000

    Error handling for database interactions passing new tests

--- __tests__/comments-db.test.js
+++ __tests__/comments-db.test.js
@@ -20,11 +20,9 @@ describe('Adding a new resource to a mongodb collection', async() => {
         commentsDB.deleteResource("mongodb://localhost:27017/comments_database",
                                            "comments",
                                            addedCommentID)
-    });    
+    })   
 
 	test('Adding a new comment inserts it into the database successfully', async done => {
-
-        expect.assertions(1)
         
         // Send a test comment object to the correct database
         const response = await commentsDB.addResourceToCollection("mongodb://localhost:27017/comments_database",
@@ -35,7 +33,8 @@ describe('Adding a new resource to a mongodb collection', async() => {
         expect(response).toBeTruthy()
         
         done()
-	})
+	})  
+
 })
 
 describe('Requesting one resource from a mongodb collection', async() => {
@@ -66,8 +65,6 @@ describe('Requesting one resource from a mongodb collection', async() => {
     })
 
 	test('Request a mongodb object returns the correct object that was requested', async done => {
-
-        expect.assertions(1)
         
         // Send a test comment object to the correct database
         const response = await commentsDB.getResourceFromCollection("mongodb://localhost:27017/comments_database",
@@ -85,12 +82,11 @@ describe('Requesting all resources from a mongodb collection', async() => {
 
 	test('Requesting a mongodb collection returns an array of objects', async done => {
 
-        expect.assertions(1)
-
         const response = await commentsDB.getAllFromCollection("mongodb://localhost:27017/comments_database",
                                                                         "comments").then((comment) => comment)      
         
-        expect(Array.isArray([response])).toBe(true);
+        expect(Array.isArray([response])).toBe(true)
+
         done()
 	})
 })
@@ -123,8 +119,6 @@ describe('Updating a resource in a mongodb collection', async() => {
     })
 
 	test('Updating a mongodb resource returns a successful response', async done => {
-
-        expect.assertions(1)
 
         const updateResponse = await commentsDB.updateResource("mongodb://localhost:27017/comments_database",
                                                                   "comments",
@@ -165,8 +159,6 @@ describe('Deleting a resource in a mongodb collection', async() => {
     })
 
 	test('Deleting a mongodb collection returns a successful response', async done => {
-
-        expect.assertions(1)
 
         const deleteResponse = await commentsDB.deleteResource("mongodb://localhost:27017/comments_database",
                                                                   "comments",
@@ -210,14 +202,81 @@ describe('Finding a resource in a mongodb collection', async() => {
 
 	test('Finding a known resource in a mongodb collection returns the correct object', async done => {
 
-        expect.assertions(1)
-
         const findResponse = await commentsDB.findResourceFromCollection("mongodb://localhost:27017/comments_database",
                                                                   "comments",
                                                                   {"comment":"test comment"}).then((response) => response)      
         
         expect(findResponse).toEqual(resourceToFind)
 
+        done()
+	})
+})
+
+describe('Requesting database interactions with incorrect credentials', async() => {
+
+    test('Adding a new comment with incorrect database credentials returns a rejected database connection', async done => {
+        
+        // Send a test comment object to the correct database
+        const response = await commentsDB.addResourceToCollection("mongodb://wrongurl:27017/comments_database",
+                                                                           "comments",
+                                                                           {"comment":"test comment"})
+                                                                           .then((result) => result)
+                                                                           .catch((reason) => reason)       
+        
+        expect(response).toEqual(Error('Unable to connect to MongoDB'))
+        
+        done()
+    })
+    
+    test('Requesting a comment with incorrect database credentials returns a rejected database connection', async done => {
+        
+        const response = await commentsDB.getResourceFromCollection("mongodb://wrongurl:27017/comments_database",
+                                                                             "comments",
+                                                                             1234)
+                                                                             .then((response) => response) 
+                                                                             .catch((reason) => reason)      
+        
+        expect(response).toEqual(Error('Unable to connect to MongoDB'))
+        
+        done()
+    })
+    
+    test('Requesting all comments with incorrect database credentials returns a rejected database connection', async done => {
+        
+        const response = await commentsDB.getAllFromCollection("mongodb://wrongurl:27017/comments_database",
+                                                                             "comments")
+                                                                             .then((response) => response) 
+                                                                             .catch((reason) => reason)      
+        
+        expect(response).toEqual(Error('Unable to connect to MongoDB'))
+        
+        done()
+    })
+    
+    test('Updating a comment with incorrect database credentials returns a rejected database connection', async done => {
+        
+        const response = await commentsDB.updateResource("mongodb://wrongurl:27017/comments_database",
+                                                                             "comments",
+                                                                             1234,
+                                                                             {"comment":"test comment"})
+                                                                             .then((response) => response) 
+                                                                             .catch((reason) => reason)      
+        
+        expect(response).toEqual(Error('Unable to connect to MongoDB'))
+        
+        done()
+	})
+    
+    test('Deleting a comment with incorrect database credentials returns a rejected database connection', async done => {
+        
+        const response = await commentsDB.deleteResource("mongodb://wrongurl:27017/comments_database",
+                                                                             "comments",
+                                                                             1234)
+                                                                             .then((response) => response) 
+                                                                             .catch((reason) => reason)      
+        
+        expect(response).toEqual(Error('Unable to connect to MongoDB'))
+        
         done()
 	})
 })
